a06086a01abec2d0aba73a3cdf323ab91346258b
Peter Keres <keres_peter@columbusstate.edu>
2019-09-21 16:09:40 -0400

    just added a few commits

--- schedual_model.c
+++ schedual_model.c
@@ -1,3 +1,19 @@
+/*
+peter keres
+sep 21 2019
+
+ file for the schedualing modle
+the schedualing model will handle any schedualing events that need
+to happen in the csu batch system.
+
+this once other classes send this the name of the batch job, it will go find it
+put it in the queuw
+
+also in chagre of changing the que order when a policiy change happens.
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "schedual_model.h"
--- schedual_model.h
+++ schedual_model.h
@@ -1,1 +1,12 @@
+/*
+peter keres
+sep 21 2019
+
+header file for the schedualing modle
+the schedualing model will handle any schedualing events that need
+to happen in the csu batch system.
+*/
+
+
+
 void test(void * num);
