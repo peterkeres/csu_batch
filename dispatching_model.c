f3fe5ee2a92c9ab4f7e89a3d3c14314ae2e6edca
Peter Keres <keres_peter@columbusstate.edu>
2019-09-21 23:10:44 -0400

    made another process call the batch file. comes back to main

--- dispatching_model.c
+++ dispatching_model.c
@@ -9,13 +9,29 @@ this ill hold the 'execv' fucntion that will run the batch file given
 should also send off any data to the 'performace modeul' as needed
 */
 
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
 
 void start(void * args[]){
 
   puts("in the dispatching_module, about to run the other program");
 
 
-  execv(args[0], args);
-  
+  pid_t pid;
+
+  pid = fork();
+
+  // if a error happend in the fork 
+  if (pid == -1) {
+    puts("error happend when making the fork");
+  // child process, we let it call the other program
+  } else if (pid == 0) {
+    execv(args[0], args);
+  }
+  // parent/ calling process. have it wait untill child is done
+  else{
+    wait(NULL);
+  }
+
 }
